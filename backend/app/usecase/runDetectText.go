5eb9026938d977e26d109170f9b54ff0a687da26
a-x-hn <alexhan0324@gmail.com>
2020-09-09 16:04:03 -0700

    update

--- backend/app/usecase/runDetectText.go
+++ backend/app/usecase/runDetectText.go
@@ -1,8 +1,18 @@
 package usecase
 
-// import "github.com/aws/aws-lambda-go/lambda"
+// import (
+// 	entity/rawimage
+// 	"fmt"
+// 	"context"
+// 	"github.com/aws/aws-lambda-go/lambda"
+// )
+
+// HandleLambdaEvent takes RawImage, runs AWS Rekog store it temp in S3 bucket and return S3 bucket address in String
+func HandleLambdaEvent(event RawImage) (string, error) {
+	//stub
+}
 
 // RunDetectText invokes lambda function to run DetectText with AWS Rekognition
 func runDetectText() {
-	//stub;
+	lambda.Start(HandleLambdaEvent)
 }
