a7df78cda1fb800bddf1b63c0fba54790b93bd5b
a-x-hn <alexhan0324@gmail.com>
2020-09-09 15:34:37 -0700

    basic setup for AWS lambda functions for usecases

---
+++ backend/app/usecase/retrieveResults.go
@@ -0,0 +1,8 @@
+package usecase
+
+// import ""
+
+// RetrieveResults invokes a lambda function that retrieves the relevant ParsedImage from the S3 bucket
+func retriveResults() {
+	// stub;
+}
---
+++ backend/app/usecase/runDetectText.go
@@ -0,0 +1,8 @@
+package usecase
+
+// import "github.com/aws/aws-lambda-go/lambda"
+
+// RunDetectText invokes lambda function to run DetectText with AWS Rekognition
+func runDetectText() {
+	//stub;
+}
