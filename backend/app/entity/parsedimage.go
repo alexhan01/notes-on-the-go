c678676a7942f3e61a0d9a946830175958783c9e
a-x-hn <alexhan0324@gmail.com>
2020-09-09 15:26:52 -0700

    adding basic entities

---
+++ backend/app/entity/parsedimage.go
@@ -0,0 +1,9 @@
+package entity
+
+// import ""
+
+// ParsedImage represents the image after text detection
+type ParsedImage struct {
+	//stub;
+	Location string
+}
---
+++ backend/app/entity/rawimage.go
@@ -0,0 +1,9 @@
+package entity
+
+// import ""
+
+// RawImage represents the image before text detection
+type RawImage struct {
+	//stub;
+	Location string
+}
