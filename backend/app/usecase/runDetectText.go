044cf9660e9a5f551a532d6b8089845dccd85ac0
jindaboi <jindazhao01@gmail.com>
2020-09-11 18:46:40 -0700

    changed

--- backend/app/usecase/runDetectText.go
+++ backend/app/usecase/runDetectText.go
@@ -7,6 +7,7 @@ func runDetectText() {
 
 	// 		helper: run retrieveFront() -> just get image from directory
 	//		helper: run rekognition() return the repsonse.convertToJsonFile()
+
 	//		helper: run parseJson() return the file
 	//		helper: run returnFront() -> store back into data folder
 	//
