6db0bd95e5677abad430660f26036920b8daebe8
jindaboi <jindazhao01@gmail.com>
2020-09-12 14:38:54 -0700

    First Attempt at writing functions

--- backend/app/usecase/runDetectText.go
+++ backend/app/usecase/runDetectText.go
@@ -1,16 +1,45 @@
 package usecase
 
-// import "github.com/aws/aws-lambda-go/lambda"
+// import (
+// 	entity/rawimage
+// 	"fmt"
+// 	"context"
+// 	"github.com/aws/aws-lambda-go/lambda"
+//	"os"
+// 	import "github.com/aws/aws-sdk-go/service/rekognition"
+// )
+
+// HandleLambdaEvent takes RawImage, runs AWS Rekog store it temp in S3 bucket and return S3 bucket address in String
+func HandleLambdaEvent(event RawImage) (string, error) {
+	retrieveFront(RawImage)
+}
 
 // RunDetectText invokes lambda function to run DetectText with AWS Rekognition
 func runDetectText() {
+	lambda.Start(HandleLambdaEvent)
+	
 
-	// 		helper: run retrieveFront() -> just get image from directory
-	//		helper: run rekognition() return the repsonse.convertToJsonFile()
+}
 
-	//		helper: run parseJson() return the file
-	//		helper: run returnFront() -> store back into data folder
-	//
+// helper function to retrieve image from dummy file
+func retrieveFront() {
+	location := RawImage.location
+	rekognition(location)
+}
 
-	//stub;
+//passes image into rekognition()
+func rekognition(string location) {
+
+	// Read image from file that already exists
+	existingImageFile, err := os.Open(location)
+	if err != nil {
+		// Handle error
+	}
+	defer existingImageFile.Close()
+
+	//do the acutal shit
+	rekognition := new AWS.Rekognition()
+	return rekognition.DetectText(existingImageFile)// idk how to write the error handling in go
+
+
 }
