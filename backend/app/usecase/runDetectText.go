8afb7159c01c22e754e796f8618ed82bdd54ead2
jindaboi <jindazhao01@gmail.com>
2020-09-11 18:41:27 -0700

    new changes

--- backend/app/usecase/runDetectText.go
+++ backend/app/usecase/runDetectText.go
@@ -4,5 +4,12 @@ package usecase
 
 // RunDetectText invokes lambda function to run DetectText with AWS Rekognition
 func runDetectText() {
+
+	// 		helper: run retrieveFront() -> just get image from directory
+	//		helper: run rekognition() return the repsonse.convertToJsonFile()
+	//		helper: run parseJson() return the file
+	//		helper: run returnFront() -> store back into data folder
+	//
+
 	//stub;
 }
